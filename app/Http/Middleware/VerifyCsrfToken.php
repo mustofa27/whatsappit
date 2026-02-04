d1ddd6fe90d0f8ba368f590833a36356fd1a0bdb
Ahmad Mustofa <mustofaahmad@poltera.ac.id>
2026-02-04 20:44:13 +0700

    fixing bug

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,7 +12,9 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'webhook/xendit',
-        'webhook/meta',
+        '/webhook/xendit',
+        '/webhook/meta',
+        'webhook/xendit*',
+        'webhook/meta*',
     ];
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->use([
+        $middleware->web(append: [
             \App\Http\Middleware\VerifyCsrfToken::class,
         ]);
         
